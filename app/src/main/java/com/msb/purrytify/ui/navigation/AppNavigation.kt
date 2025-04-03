1279050d8ed00ef4232b4540944dc20f99494d42
Nicholas Sihite <nicholassihite05@gmail.com>
2025-04-03 23:00:48 +0700

    feat: navbar that match figma design

--- app/src/main/java/com/msb/purrytify/ui/navigation/AppNavigation.kt
+++ app/src/main/java/com/msb/purrytify/ui/navigation/AppNavigation.kt
@@ -2,28 +2,52 @@ package com.msb.purrytify.ui.navigation
 
 import android.content.res.Configuration
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Icon
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Home
-import androidx.compose.material.icons.filled.Menu
-import androidx.compose.material.icons.filled.Person
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
-import com.msb.purrytify.MainActivity
+//import com.msb.purrytify.MainActivity
 import com.msb.purrytify.ui.component.NavigationBarComponent
 import com.msb.purrytify.ui.screen.*
 import com.msb.purrytify.ui.theme.AppTheme
+import androidx.compose.ui.unit.dp
+import com.msb.purrytify.R
+import androidx.compose.ui.res.painterResource
 
 
-sealed class Screen(val route: String, val label: String, val icon: @Composable () -> Unit) {
-    data object Home : Screen("home", "Home", { Icon(Icons.Filled.Home, contentDescription = "Home") })
-    data object Library : Screen("library", "Library", { Icon(Icons.Filled.Menu, contentDescription = "Library") })
-    data object Profile : Screen("profile", "Profile", { Icon(Icons.Filled.Person, contentDescription = "Profile") })
+sealed class Screen(val route: String, val label: String, val icon: @Composable (isSelected: Boolean) -> Unit) {
+    data object Home : Screen("home", "Home", { isSelected ->
+        Icon(
+            painter = painterResource(
+                id = if (isSelected) R.drawable.home_bold else R.drawable.home
+            ),
+            contentDescription = "Home",
+            modifier = Modifier.size(24.dp)
+        )
+    })
+    data object Library : Screen("library", "Your Library", { isSelected ->
+        Icon(
+            painter = painterResource(
+                id = if (isSelected) R.drawable.library_bold else R.drawable.library
+            ),
+            contentDescription = "Your Library",
+            modifier = Modifier.size(24.dp)
+        )
+    })
+    data object Profile : Screen("profile", "Profile", { isSelected ->
+        Icon(
+            painter = painterResource(
+                id = if (isSelected) R.drawable.profile_bold else R.drawable.profile
+            ),
+            contentDescription = "Profile",
+            modifier = Modifier.size(24.dp)
+        )
+    })
     data object Login : Screen("login", "Login", {})
 }
 
