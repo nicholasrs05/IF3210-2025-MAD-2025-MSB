9e741770d1792c6434cfcbb76ea56a9158e09e7a
maulvizm <13522122@std.stei.itb.ac.id>
2025-04-04 19:13:03 +0700

    feat: create login screen and added navigation logic

--- app/src/main/java/com/msb/purrytify/ui/navigation/AppNavigation.kt
+++ app/src/main/java/com/msb/purrytify/ui/navigation/AppNavigation.kt
@@ -9,15 +9,16 @@ import androidx.compose.material.icons.filled.Home
 import androidx.compose.material.icons.filled.Menu
 import androidx.compose.material.icons.filled.Person
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
-import com.msb.purrytify.MainActivity
 import com.msb.purrytify.ui.component.NavigationBarComponent
 import com.msb.purrytify.ui.screen.*
-import com.msb.purrytify.ui.theme.AppTheme
+import com.msb.purrytify.viewmodel.AuthViewModel
 
 
 sealed class Screen(val route: String, val label: String, val icon: @Composable () -> Unit) {
@@ -35,19 +36,22 @@ sealed class Screen(val route: String, val label: String, val icon: @Composable 
     uiMode = Configuration.UI_MODE_NIGHT_NO,
     name = "DefaultPreviewLight"
 )
+
 @Composable
-fun NavigationComponent() {
-    AppTheme {
-        val navController = rememberNavController()
-        Scaffold(
-            bottomBar = { NavigationBarComponent(navController) }
-        ) { innerPadding ->
-            NavHost(navController, startDestination = Screen.Home.route, Modifier.padding(innerPadding)) {
-                composable(Screen.Home.route) { HomeScreen() }
-                composable(Screen.Library.route) { LibraryScreen() }
-                composable(Screen.Profile.route) { ProfileScreen(navController) }
-                composable(Screen.Login.route) { LoginScreen(navController) }
-            }
+fun NavigationComponent(authViewModel: AuthViewModel = hiltViewModel()) {
+    val navController = rememberNavController()
+    val isLoggedIn = authViewModel.uiState.collectAsState().value.isLoggedIn
+
+    val startDestination = if (isLoggedIn) Screen.Home.route else Screen.Login.route
+
+    Scaffold(
+        bottomBar = { if (isLoggedIn) NavigationBarComponent(navController) else null }
+    ) { innerPadding ->
+        NavHost(navController, startDestination = startDestination, Modifier.padding(innerPadding)) {
+            composable(Screen.Home.route) { HomeScreen() }
+            composable(Screen.Library.route) { LibraryScreen() }
+            composable(Screen.Profile.route) { ProfileScreen() }
+            composable(Screen.Login.route) { LoginScreen(navController) }
         }
     }
 }
--- app/src/main/java/com/msb/purrytify/ui/screen/ProfileScreen.kt
+++ app/src/main/java/com/msb/purrytify/ui/screen/ProfileScreen.kt
@@ -3,18 +3,10 @@ package com.msb.purrytify.ui.screen
 import androidx.compose.foundation.layout.Column
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.navigation.NavHostController
-import com.msb.purrytify.ui.navigation.Screen
 
 @Composable
-fun ProfileScreen(navController: NavHostController) {
-    val isAuthenticated = false //Dummy auth
-
-    if (isAuthenticated) {
-        Column {
-            Text("Profile Screen")
-        }
-    } else {
-        navController.navigate(Screen.Login.route)
+fun ProfileScreen() {
+    Column {
+        Text("Profile Screen")
     }
 }
