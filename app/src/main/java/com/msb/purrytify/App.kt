9d89f59c0e020ab839b74ce0b6c082cd493f153e
maulvizm <13522122@std.stei.itb.ac.id>
2025-04-09 22:01:41 +0700

    feat: add background service to update token

--- app/src/main/java/com/msb/purrytify/App.kt
+++ app/src/main/java/com/msb/purrytify/App.kt
@@ -1,8 +1,48 @@
 package com.msb.purrytify
 
 import android.app.Application
+import androidx.hilt.work.HiltWorkerFactory
+import androidx.work.Configuration
+import androidx.work.Constraints
+import androidx.work.ExistingPeriodicWorkPolicy
+import androidx.work.NetworkType
+import androidx.work.PeriodicWorkRequestBuilder
+import androidx.work.WorkManager
+import com.msb.purrytify.service.RefreshTokenService
 import dagger.hilt.android.HiltAndroidApp
+import java.util.concurrent.TimeUnit
+import javax.inject.Inject
 
 @HiltAndroidApp
-class Purritify : Application() {
+class Purritify : Application(), Configuration.Provider {
+    @Inject
+    lateinit var workerFactory: HiltWorkerFactory
+
+    override fun onCreate() {
+        super.onCreate()
+        scheduleRefreshTokenWork()
+    }
+
+    private fun scheduleRefreshTokenWork() {
+        val constraints = Constraints.Builder()
+            .setRequiredNetworkType(NetworkType.CONNECTED)
+            .setRequiresBatteryNotLow(true)
+            .build()
+
+        val refreshTokenRequest = PeriodicWorkRequestBuilder<RefreshTokenService>(
+            15, TimeUnit.MINUTES
+        ).setConstraints(constraints)
+            .build()
+
+        WorkManager.getInstance(applicationContext).enqueueUniquePeriodicWork(
+            "RefreshTokenWork",
+            ExistingPeriodicWorkPolicy.KEEP,
+            refreshTokenRequest
+        )
+    }
+
+    override val workManagerConfiguration: Configuration
+        get() = Configuration.Builder()
+            .setWorkerFactory(workerFactory)
+            .build()
 }
---
+++ app/src/main/java/com/msb/purrytify/service/RefreshTokenService.kt
@@ -0,0 +1,44 @@
+package com.msb.purrytify.service
+
+import android.content.Context
+import androidx.hilt.work.HiltWorker
+import androidx.work.CoroutineWorker
+import androidx.work.WorkerParameters
+import com.msb.purrytify.data.api.ApiService
+import com.msb.purrytify.data.storage.DataStoreManager
+import dagger.assisted.Assisted
+import dagger.assisted.AssistedInject
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.withContext
+import retrofit2.HttpException
+
+@HiltWorker
+class RefreshTokenService @AssistedInject constructor(
+    @Assisted appContext: Context,
+    @Assisted workerParams: WorkerParameters,
+    private val apiService: ApiService,
+    private val dataStoreManager: DataStoreManager
+) : CoroutineWorker(appContext, workerParams) {
+
+    override suspend fun doWork(): Result = withContext(Dispatchers.IO) {
+        val currentJwt = dataStoreManager.authTokenFlow.first()
+
+        if (currentJwt.isNullOrBlank()) {
+            return@withContext Result.success()
+        }
+
+        try {
+            val verificationResponse = apiService.verifyToken()
+            if (verificationResponse.isSuccessful) {
+                return@withContext Result.success()
+            } else {
+                return@withContext Result.failure()
+            }
+        } catch (e: HttpException) {
+            return@withContext Result.failure()
+        } catch (e: Exception) {
+            return@withContext Result.failure()
+        }
+    }
+}
