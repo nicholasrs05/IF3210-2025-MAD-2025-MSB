91b6ed2caf30877706d9b35389edcb9263ce70a1
maulvizm <13522122@std.stei.itb.ac.id>
2025-04-09 18:20:47 +0700

    fix: change navigation logic and adding blocking splash screen

--- app/src/main/java/com/msb/purrytify/MainActivity.kt
+++ app/src/main/java/com/msb/purrytify/MainActivity.kt
@@ -1,11 +1,20 @@
 package com.msb.purrytify
 
 import android.os.Bundle
+import android.util.Log
+import android.view.ViewTreeObserver
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import dagger.hilt.android.AndroidEntryPoint
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.DisposableEffect
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.platform.LocalView
+import androidx.hilt.navigation.compose.hiltViewModel
 import com.msb.purrytify.ui.navigation.NavigationComponent
 import com.msb.purrytify.ui.theme.AppTheme
+import com.msb.purrytify.viewmodel.AuthViewModel
+import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
@@ -13,8 +22,46 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             AppTheme {
-                NavigationComponent()
+                MainContent()
             }
         }
     }
+}
+
+@Composable
+fun MainContent() {
+    val authViewModel: AuthViewModel = hiltViewModel()
+    val uiState by authViewModel.uiState.collectAsState()
+    val isLoggedInCheckDone = uiState.isLoggedInCheckDone
+
+    // Add debugging log
+    Log.d("MainActivity", "isLoggedInCheckDone: $isLoggedInCheckDone")
+
+    val localView = LocalView.current
+
+    DisposableEffect(Unit) {
+        val preDrawListener = object : ViewTreeObserver.OnPreDrawListener {
+            override fun onPreDraw(): Boolean {
+                // Log.d("MainActivity", "onPreDraw called, isLoggedInCheckDone: $isLoggedInCheckDone")
+                return if (authViewModel.isLoggedInCheckDone()) {
+                    // Log.d("MainActivity", "Removing preDrawListener and allowing draw")
+                    localView.viewTreeObserver.removeOnPreDrawListener(this)
+                    true
+                } else {
+                    // Log.d("MainActivity", "Preventing draw, waiting for auth check")
+                    false
+                }
+            }
+        }
+
+        // Log.d("MainActivity", "Adding preDrawListener")
+        localView.viewTreeObserver.addOnPreDrawListener(preDrawListener)
+
+        onDispose {
+            Log.d("MainActivity", "Disposing and removing preDrawListener")
+            localView.viewTreeObserver.removeOnPreDrawListener(preDrawListener)
+        }
+    }
+
+    NavigationComponent()
 }
